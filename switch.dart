2db3f0f5271298fd95d581378f59d9ba02f390c8
Sanjay R <139671871+sanjayr1944@users.noreply.github.com>
2024-07-07 13:05:21 +0530

    Add files via upload

---
+++ switch.dart
@@ -0,0 +1,33 @@
+void main()
+{
+  var Day=7;
+  switch(Day) {
+    case 1:
+      print('Its Monday,Weekday');
+      break;
+    case 2:
+      print('Its Tuesday,Weekday');
+      break;
+    case 3:
+      print('Its Wednesday,Weekday');
+      break;
+    case 4:
+      print('Its Thursday,Weekday');
+      break;
+    case 5:
+      print('Its Friday,Weekday');
+      break;
+    case 6:
+      print('Its Saturday,Weekend');
+      break;
+    case 7:
+      print('Its Sunday,Weekend');
+      break;
+    default:
+      print('Invalid Day Entered');
+
+
+  }
+
+
+}
